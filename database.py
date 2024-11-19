5915c9226029b418a91b55ecc91da4e2a99a78f2
SumanAsim22 <111068114+SumanAsim22@users.noreply.github.com>
2024-11-19 03:25:33 +0300

    Initial commit

---
+++ database.py
@@ -0,0 +1,312 @@
+"""
+Database module for the Habit Tracker application.
+
+This module provides the functionality for establishing a 
+database connection and executing SQL queries for the
+habit, task, and streak tables.
+
+Functions
+---------
+get_db(str) -> sqlite3.Connection:
+    Establishes connection to local database.
+execute_query(str, tuple(optional)) -> None
+    Executes a SQL query (except SELECT queries).
+execute_select_query(str, tuple(optional)) -> list
+    Executes a SQL SELECT query.
+create_tables() -> None
+    Creates the database tables.
+add_habit() -> None
+    Adds a new habit to the habit table.
+add_task_checkoff() -> None
+    Adds a new task checkoff to the task table.
+add_streak() -> None
+    Adds a new streak to the streak table.
+get_all_habits() -> list
+    Gets all the habits from the habit table.
+get_task_list(str) -> list
+    Gets all the task checkoffs for a habit.
+get_streak_list(str) -> list
+    Gets all the streaks for a habit.
+update_streak_counter(str, datetime.date) -> None
+    Updates the latest streak counter of a habit.
+delete_habit_from_db(str) -> None
+    Deletes a habit and all associated entries in task and streak tables.
+
+Notes
+-----
+- The application relies on the `sqlite3` module for database operations.
+"""
+import sqlite3
+
+def get_db(db_name='test.db') -> sqlite3.Connection:
+    """
+    Establishes connection to local database.
+
+    Parameters
+    ----------
+    db_name : str
+        Name of the database to connect to.
+
+    Returns
+    -------
+    sqlite3.Connection
+        The database connection object.
+    """
+    db = sqlite3.connect(db_name)
+    return db
+
+def execute_query(query: str, values: tuple = None): 
+    """
+    Executes a SQL query (except SELECT queries).
+     
+    Prevents redundant code for committing and closing the database
+    when executing SQL queries, as well as error handling.
+    execute_select_query() is used for SELECT queries.
+
+    Parameters
+    ----------
+    query : str
+        The SQL query to execute.
+    values : tuple, optional
+        The values to be passed to the SQL query. 
+
+    Returns
+    -------
+    None
+    """
+    db = get_db() 
+    cur = db.cursor() 
+    try: 
+        if values: #if values are passed
+            cur.execute(query, values) 
+        else: 
+            cur.execute(query) 
+        db.commit() 
+    except Exception as e: 
+        print(f"An error occurred: {e}") 
+        db.rollback()
+    finally: 
+        db.close()
+    return db
+
+def execute_select_query(query: str, values: tuple = None) -> list: 
+    """
+    Executes a SQL SELECT query.
+     
+    Prevents redundant code for committing and closing the database
+    when executing SQL SELECT queries, as well as error handling.
+    Separated from execute_query() to receive query results
+    instead of connection object.
+
+    Parameters
+    ----------
+    query : str
+        The SQL SELECT query to execute.
+    values : tuple, optional
+        The values to be passed to the SQL query. 
+
+    Returns
+    -------
+    list
+        The query result as a list of rows.
+        Each row is represented as a tuple.
+    """
+    db = get_db() 
+    cur = db.cursor() 
+    try: 
+        if values: 
+            cur.execute(query, values) 
+        else: 
+            cur.execute(query) 
+        results = cur.fetchall() 
+        return results 
+    except Exception as e: 
+        print(f"An error occurred: {e}") 
+        return None 
+    finally: 
+        db.close()
+
+def create_tables() -> None:
+    """
+    Creates the database tables if they do not already exist.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    # table creation queries for each table
+    habit_table_query = """CREATE TABLE IF NOT EXISTS habit(
+                        title TEXT PRIMARY KEY,
+                        description TEXT,
+                        frequency TEXT,
+                        creation_date DATE
+                    )"""
+    task_table_query = """CREATE TABLE IF NOT EXISTS task(
+                        habit_title TEXT,
+                        checkoff_date DATE,
+                        FOREIGN KEY (habit_title) REFERENCES habit(title)
+                    )"""
+    streak_table_query = """CREATE TABLE IF NOT EXISTS streak(
+                        habit_title TEXT,
+                        counter INTEGER,
+                        last_updated DATE,
+                        FOREIGN KEY (habit_title) REFERENCES habit(title)
+                    )"""
+    # execute table creation queries
+    execute_query(habit_table_query) 
+    execute_query(task_table_query) 
+    execute_query(streak_table_query)
+
+def add_habit(self) -> None:
+    """
+    Adds a new habit to the habit table in the database.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    add_habit_query = "INSERT INTO habit (title, description, frequency, creation_date) VALUES (?, ?, ?, ?)"
+    values = (self.title, self.descr, self.freq, self.creation_date)
+    return execute_query(add_habit_query, values)
+
+def add_task_checkoff(self) -> None:
+    """
+    Adds a new task checkoff to the task table in the database.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    add_task_query = "INSERT INTO task (habit_title, checkoff_date) VALUES (?, ?)" 
+    values = (self.habit_title, self.checkoff_date)
+    return execute_query(add_task_query, values)
+
+def add_streak(self): 
+    """
+    Adds a new streak to the streak table in the database.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+    """
+    add_streak_query = "INSERT INTO streak (habit_title, counter, last_updated) VALUES (?, ?, ?)"
+    values = (self.habit_title, self.counter, self.last_updated)
+    return execute_query(add_streak_query, values)
+
+def get_all_habits() -> list:
+    """
+    Gets all the habits from the habit table.
+
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    list
+        The list of all habits.
+        Each habit in the list is represented as a tuple.
+    """
+    get_habit_query = "SELECT * FROM habit"
+    return execute_select_query(get_habit_query)
+
+def get_task_list(habit_title: str) -> list:
+    """
+    Gets all the task checkoffs for a habit from the task table.
+
+    Parameters
+    ----------
+    habit_title : str
+        The habit for which to retrieve the task checkoff list.
+
+    Returns
+    -------
+    list
+        The list of all task checkoffs for the specified habit.
+        Each entry is represented as a tuple.
+    """
+    get_task_list_query = "SELECT * FROM task WHERE habit_title = ?"
+    values = (habit_title,)
+    return execute_select_query(get_task_list_query, values)
+
+def get_streak_list(habit_title: str) -> list:
+    """
+    Gets all the streaks for a habit from the streak table.
+
+    Parameters
+    ----------
+    habit_title : str
+        The habit for which to retrieve the streaks.
+
+    Returns
+    -------
+    list
+        The list of all streaks for the specified habit.
+        Each entry is represented as a tuple.
+    """
+    get_streak_list_query = "SELECT * FROM streak WHERE habit_title = ?"
+    values = (habit_title,)    
+    return execute_select_query(get_streak_list_query, values)
+
+def update_streak_counter(habit_title: str, date) -> None:
+    """
+    Updates the latest streak counter of a habit.
+
+    Increments the counter value and updates the date.
+
+    Parameters
+    ----------
+    habit_title : str
+        The habit for which to update the streak.
+    date : datetime.date
+        The new date to be set.
+
+    Returns
+    -------
+    None
+    """
+    # find latest streak counter for given habit title and update the values
+    update_streak_query = """UPDATE streak SET counter = counter + 1, last_updated = ?
+                WHERE last_updated = (SELECT last_updated FROM streak WHERE habit_title = ? 
+                ORDER BY last_updated DESC LIMIT 1)"""
+    values = (date, habit_title)
+    return execute_query(update_streak_query, values)
+
+def delete_habit_from_db(title: str) -> None:
+    """
+    Deletes a habit and all associated entries in task and streak tables.
+
+    Parameters
+    ----------
+    habit_title : str
+        The title of the habit to be deleted.
+    
+    Returns
+    -------
+    None
+    """
+    # deletion queries for each occurence of the habit in all tables
+    delete_habit_query = "DELETE FROM habit WHERE title = ?"
+    delete_task_query = "DELETE FROM task WHERE habit_title = ?"
+    delete_streak_query = "DELETE FROM streak WHERE habit_title = ?"
+    values = (title,)
+    
+    execute_query(delete_habit_query, values)
+    execute_query(delete_task_query, values)
+    execute_query(delete_streak_query, values)
---
+++ main.py
@@ -0,0 +1,281 @@
+"""
+Main module for the Habit Tracker application.
+
+This module serves as the entry point for the Habit Tracker application. 
+It initializes the database, defines the main application logic, 
+and handles user interactions.
+
+Functions
+---------
+is_number(str) -> bool
+    Verifies whether a value is an integer.
+run_application() -> None
+    Starts the main application.
+"""
+
+import questionary
+from rich.style import Style
+from rich.console import Console
+from rich.table import Table
+from habits import Habit, Task
+from database import get_db, create_tables, get_all_habits
+from analyse import (task_counter, check_streak, update_streak, delete_habit,  
+                     get_longest_streak, filter_habits, get_active_streak_counter)
+
+# start database connection and load tables
+get_db('test.db')
+create_tables()
+# style variables for different output types
+cancel_style = Style(color='red')
+info_style= Style(color='green4', bold=True)
+title_style = Style(color='turquoise4', bold=True)
+console = Console()
+
+def is_number(value: str) -> bool: 
+    """
+    Verifies whether a value is an integer.
+
+    Parameters
+    ----------
+    value : str
+        The value to be converted and verified.
+    
+    Returns
+    -------
+    bool
+        True if value is an integer False if not
+    """
+    try: 
+        int(value) 
+        return True 
+    except ValueError: 
+        return False
+
+def run_application() -> None:
+    """
+    Starts the main application.
+
+    Asks the user for confirmation before presenting the main menu
+    for the next action. The application runs until the user 
+    selects 'Exit'. 
+    Enables all the application functionality through 
+    CLI prompts and menus.
+    
+    Parameters
+    ----------
+    None
+
+    Returns
+    -------
+    None
+
+    Notes
+    -----
+    Methods from the Questionary and Rich libraries are used for 
+    user prompts and formatted outputs, respectively.
+    """
+    # define option lists for selection menus
+    main_menu_options = ['Create new habit', 'Manage habits', 'Analyse habits', 'Exit']
+    frequency_options = ['Daily', 'Weekly']
+    habit_management_options = ['Checkoff habit', 'Delete habit', 'Return to main menu']
+    habit_analysis_options = ['Filter habits', 'Get longest streak', 'Return to main menu']
+    filter_options = ['Frequency', 'Streak status', 'Checkoff count', 'Cancel']
+    streak_status_options = ['Active', 'Inactive']
+ 
+    # ask user for confirmation before starting the application
+    is_user_ready = questionary.confirm('Would you like to start the application?', default=True).ask()
+    # continue running the application until user selects 'Exit'
+    while is_user_ready:
+        # get habit list and titles in every iteration to sync with any updates made in the previous iteration
+        habit_list = get_all_habits() 
+        habit_titles = [habit[0] for habit in habit_list]
+        title_options = habit_titles + ['Cancel']
+
+        print('') # insert blank line
+        
+        # display the main menu in each iteration
+        main_menu = questionary.select('Main menu:', 
+                                    choices=main_menu_options).ask()
+        # 'Create new habit' option
+        if main_menu == 'Create new habit':
+            console.print('\n-----New habit form-----', style=title_style)
+            # display habit creation form and get individual values
+            habit_details = questionary.form(
+                title = questionary.text('Enter habit title: ',
+                                        # use questionary's validator to prevent empty title
+                                        validate=lambda text: True if len(text) > 0 else "Please enter a title"),
+                descr = questionary.text('Enter habit description: '),
+                freq = questionary.select('Select habit frequency: ', choices=frequency_options)).ask()
+            
+            # confirm habit creation
+            creation_confirmed = questionary.confirm(
+                f'Do you want to create the {habit_details['freq']} habit \'{habit_details['title']}\'?').ask()
+            if creation_confirmed:
+                # check if the habit title already exists
+                habit_exists = any(habit_details['title'] == habit[0] for habit in habit_list)
+                if habit_exists:
+                    console.print('Habit title already exists - please enter a different title', style=cancel_style)
+                else:
+                    # create new habit object
+                    Habit(habit_details['title'], habit_details['descr'], habit_details['freq'])
+                    console.print(f'New habit created: {habit_details['title']}', style=info_style)
+            else: console.print('Habit creation cancelled by user', style=cancel_style)
+        
+        # 'Manage habits' option 
+        elif main_menu == 'Manage habits':
+            # execute if a habit list is retrieved successfully
+            if habit_list: 
+                for title in habit_titles:
+                    if not check_streak(title):
+                        update_streak(title) 
+
+                # create and display a table of all habits in the database
+                table = Table(title='Habit List')
+                table.add_column('Title', justify='left', style='cyan', no_wrap=True)
+                table.add_column('Description', style='magenta', no_wrap=True)
+                table.add_column('Frequency', style='green', no_wrap=True)
+                table.add_column('Current streak', justify='center', style='yellow', no_wrap=True)
+                table.add_column('Checkoff count', justify='center', style='dark_orange', no_wrap=True)
+
+                for habit in habit_list:
+                    title, descr, freq, creation_date = habit
+                    # get active streak counter to get latest streak count
+                    active_counter = get_active_streak_counter(title)
+                    if active_counter[1] == 1:
+                        """
+                        If no streak has been established, the default value is 1 in order
+                        to account for the first day of the streak when it starts.
+                        However, this value is set to 0 when being displayed to reflect that
+                        no streak has been established yet.
+                        """
+                        streak_count = 0
+                    else: 
+                        streak_count = active_counter[1]
+                    task_count = task_counter(title) #get number of checkoffs for habit
+                    table.add_row(title, descr, freq, str(streak_count), str(task_count))
+                console.print(table)
+
+                # display next set of actions relating to user's habit list
+                menu = questionary.select('\nWhat would you like to do?', 
+                                    choices=habit_management_options).ask()
+
+                # 'Checkoff habit' option
+                if menu == 'Checkoff habit':
+                    habit_title = questionary.select('Select a habit to checkoff', 
+                                        choices=title_options).ask()
+                    if habit_title == 'Cancel':
+                        console.print('Task checkoff cancelled', style=cancel_style)
+                    else: 
+                        # create new task object if habit is checked off and update its streak 
+                        Task(habit_title)
+                        update_streak(habit_title)
+                        console.print(f'Habit checked off: {habit_title}', style=info_style)
+
+                # 'Delete habit' option
+                elif menu == 'Delete habit':
+                    habit_title = questionary.select('Select a habit to delete', 
+                                        choices=title_options).ask()
+                    if habit_title == 'Cancel':
+                        console.print('Habit deletion cancelled by user', style=cancel_style)
+                    else:
+                        deletion_confirmed = questionary.confirm(
+                            'Are you sure you want to delete this habit? This action cannot be reversed!').ask()
+                        if deletion_confirmed: 
+                            delete_habit(habit_title)
+                            console.print(f'Habit \'{habit_title}\' deleted successfully', style=info_style)
+                        else: 
+                            console.print('Habit deletion cancelled by user', style=cancel_style)
+                
+                # 'Return to main menu' option
+                else: 
+                    pass
+            else:
+                console.print('No habits found', style=cancel_style)
+
+        # 'Analyse habits' option
+        elif main_menu == 'Analyse habits':
+            # execute if a habit list is retrieved successfully
+            if habit_list:
+                console.print('\n-----Habit analysis-----', style=title_style)
+                analysis_function = questionary.select('Please choose a function:',
+                                                choices=habit_analysis_options).ask()
+                empty_list_msg = 'No habits found with this filter'
+
+                if analysis_function == 'Filter habits':
+                    filter = questionary.select('Select filter: ', choices=filter_options).ask()
+                    
+                    # find and return habits with the selected frequency
+                    if filter == 'Frequency':
+                        freq = questionary.select('Select habit frequency: ', choices=frequency_options).ask()
+                        matched_list = filter_habits(freq)
+
+                        if matched_list == []: 
+                            console.print(empty_list_msg, style=info_style)
+                        else: 
+                            console.print(f'{freq} habits:' , style=title_style)
+                            for title in matched_list:
+                                print(title)
+
+                    # find and return habits with the selected streak status
+                    elif filter == 'Streak status':
+                        status = questionary.select('Select streak status:', 
+                                                    choices=streak_status_options).ask()
+                        matched_list = filter_habits(status)
+
+                        if matched_list == []: 
+                            console.print(empty_list_msg, style=info_style)
+                        else: 
+                            console.print(f'Habits with {status} streak:' , style=title_style)
+                            for title in matched_list:
+                                print(title)
+
+                    # find and return habits with task checkoff count at least equal to the specified value
+                    elif filter == 'Checkoff count':
+                        value = questionary.text('Enter a number (used as minimum value - e.g.: at least 2 checkoffs):',
+                                                # verify user-entered value is an integer
+                                                validate=lambda value: True if is_number(value) 
+                                                else "Please enter a valid number").ask()
+                        matched_list = filter_habits(int(value))
+
+                        if matched_list == []: 
+                            console.print(empty_list_msg, style=info_style)
+                        else: 
+                            console.print(f'Habits with at least {value} checkoff/s:', style=title_style)
+                            table = Table()
+                            table.add_column('Habit', justify='left', style='cyan', no_wrap=True)
+                            table.add_column('Count', style='magenta')
+                            for title in matched_list:
+                                count = task_counter(title)
+                                table.add_row(title, str(count))
+                            console.print(table)
+                    # 'Cancel' option                        
+                    else: 
+                        pass
+
+                elif analysis_function == 'Get longest streak': 
+                    habit_title = questionary.select('Select a habit for the function:', 
+                                        choices=['All']+title_options).ask()
+
+                    if habit_title == 'Cancel':
+                        pass
+                    elif habit_title == 'All':
+                        longest_streak = get_longest_streak(habit_title)
+                        console.print(f'\nLongest streak across all habits: {str(longest_streak[0])}', style=info_style)
+                        console.print('For habit/s:', style=title_style)
+                        for title in longest_streak[1]:
+                            print(title)
+                    else:
+                        longest_streak = get_longest_streak(habit_title)
+                        console.print(f'Longest streak for \'{habit_title}\': {str(longest_streak)}', style=info_style)
+                # 'Return to main menu' option
+                else: 
+                    pass  
+            else:
+                console.print('No habits found', style=cancel_style)
+        # 'Exit' option 
+        else:
+            is_user_ready = False #stop loop
+            console.print('Application stopped by user', style=cancel_style)
+
+if __name__ == '__main__':
+    run_application()
